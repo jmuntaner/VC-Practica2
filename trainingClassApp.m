429b104a7e59390a21eb5aaad7ad8a55d737a987
Joan Muntaner <joanmuntaner1999@gmail.com>
2020-05-21 01:30:25 +0200

    Display dels errors

--- trainingClassApp.m
+++ trainingClassApp.m
@@ -15,3 +15,17 @@ for i=1:10
 acc=acc+confMat(i,i);
 end
 acc/(X/100)
+error = find(yfit~=labs);
+s = size(error,1);
+errorLabeled = cell(s,3);
+for i=1:s
+    t = error(i);
+    errorLabeled{i,1} = imTrain(:,:,t);
+    errorLabeled{i,2} = labs(t);
+    errorLabeled{i,2} = yfit(t);
+end
+for i=1:s
+    subplot(9,14,i);
+    imshow(errorLabeled{i,1}),title("T: " + errorLabeled{i,2} + " G: " + errorLabeled{i,3});
+end
+sgtitle("Errors, T: True Label, G: Classifier Guess")
